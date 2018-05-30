f1a8261662a2296009c86a1d3c3d4001e1f8bd81
Dylan Abraham <abrahamd@cat.pdx.edu>
2018-05-30 12:25:46 -0700

    Added all but list/part

--- client/client.js
+++ client/client.js
@@ -1,8 +1,29 @@
 var server = 'ws://localhost:8080/';
-
-function write_message(message) {
-    message = message + '<br>';
-    $('#chats').append(message);
+var roomList = new Array;
+var current = "default";
+function write_message(source,target,message) {
+    var payload = '[' + target + '] ' + source + ': ' + message + '<br>';
+    $('#chats').append(payload);
+}
+function write_joined(source,target){
+    var payload = '[' + target + '] ' + source + ' has joined the channel.<br>';
+    $('#chats').append(payload);
+}
+function write_info(message){
+    var payload = '[INFO] ' + message + '<br>';
+    $('#chats').append(payload);
+}
+function write_parted(source,target){
+    var payload = '[' + target + ']' + source + ' has left the channel.<br>';
+    $('#chats').append(payload);
+}
+function write_rooms(rooms) {
+    var payload = "Rooms: ";
+    rooms.forEach(room =>{
+        payload = payload + room + ' ';
+    });
+    payload = payload + '<br>';
+    $('#chats').append(payload);
 }
 
 $(document).ready(function() {
@@ -18,24 +39,69 @@ $(document).ready(function() {
 
         socket.onopen = function(event) {
             $('#nick_box').hide();
-            write_message('Connected to: ' + server);
-            socket.send(nick);
+            write_info('Connected to: ' + server);
+            var payload = {"header":"NICK", "nick":nick};
+            socket.send(JSON.stringify(payload));
             $('#chat_wrapper').show();
             $('#chat').focus();
         };
 
         socket.onmessage = function(event) {
             console.log(event.data);
-            write_message(event.data);
-        };
+            var obj = JSON.parse(event.data);
+            switch(obj.header){
+            case "MSG":
+                write_message(obj.source, obj.target, obj.message);
+                break;
+            case "JOINED":
+                write_joined(obj.source, obj.target);
+                break;
+            case "INFO":
+                write_info(obj.message);
+                break;
+            case "PARTED":
+                write_parted(obj.source, obj.target);
+                break;
+            case "LIST":
+                write_rooms(obj.rooms);
+            }
+        }
 
         socket.onclose = function(event) {
             write_message('Disconnected from ' + server);
         };
 
         $('#chat_form').submit(function() {
-            socket.send($('#chat').val());
+            var message = $('#chat').val();
+            var obj = {"header": "MSG", "target": current, "message":message};
+            socket.send(JSON.stringify(obj));
             $('#chat').val('');
+            return false;
+        });
+        $('#defaultButton').click(function(){
+            current = "default";
+            $('.dropdown-item').removeClass('active');
+            $('#defaultButton').addClass('active');
+        })
+        $('#room_form').submit(function(){
+            var room = $('#room').val();
+            if (roomList.includes(room)) {
+                return false;
+            }
+            roomList.push(room);
+            var obj = {"header": "JOIN", "target": room}
+            socket.send(JSON.stringify(obj));
+            $('#room').val('');
+            $('#rooms').append(room + '<br>');
+            $('#chanList').append(
+                $('<li>').append(
+                    $('<button>').attr('type','button').attr('id',room+'Button').addClass('dropdown-item').append(room)
+                ));
+            $('#'+room+'Button').click(function(){
+                current = room;
+                $('.dropdown-item').removeClass('active');
+                $('#'+room+'Button').addClass('active');
+            });
             return false;
         });
 
--- server/server.py
+++ server/server.py
@@ -2,27 +2,109 @@
 
 import asyncio
 import websockets
+import json
 
 connections = {}
+rooms = ["default"]
+
+async def send_to_room(room, payload):
+    for conn in connections.keys():
+        if room in connections[conn]["rooms"]:
+            await conn.send(payload)
+
+async def send_all(payload):
+    for conn in connections.keys():
+        await conn.send(payload)
+
+def list_users(target):
+    users = []
+    for conn in connections.keys():
+        if target in connections[conn]["rooms"]:
+            users.push(connections[conn]["nick"])
+    return users
+
 async def connection_handler(websocket,path):
     try:
         print("New connection from", websocket)
-        nick = await websocket.recv()
+        message = await websocket.recv()
+        print(message)
+        message_obj = json.loads(message)
+        nick = message_obj["nick"]
         connections[websocket] = {
-            "nick": nick
+            "nick": nick,
+            "rooms": ["default"]
         }
         print("Got nick {}".format(nick))
-        for conn in connections.keys():
-            await conn.send("{} has joined the chat room!".format(nick))
+        payload = json.dumps({
+            "header":"JOINED",
+            "target":"default",
+            "source":nick
+        })
+        await send_to_room("default", payload)
         async for message in websocket:
-            print("Got message {}".format(message))
-            for conn in connections.keys():
-                await conn.send("{}: {}".format(nick,message))
+            message_obj = json.loads(message)
+            header = message_obj["header"]
+            if header == "MSG":
+                target = message_obj["target"]
+                payload = json.dumps({
+                    "header":"MSG",
+                    "source":nick,
+                    "target":target,
+                    "message":message_obj["message"]
+                })
+                await send_to_room(target, payload)
+            elif header == "NICK":
+                oldnick = nick
+                nick = message_obj["nick"]
+                connections[websocket]["nick"]=nick
+                payload = json.dumps({
+                    "header":"INFO",
+                    "message": "{} is now known as {}".format(oldnick,nick)
+                })
+                await send_all(payload)
+            elif header == "JOIN":
+                target = message_obj["target"]
+                if target not in rooms:
+                    rooms.append(target)
+                connections[websocket]["rooms"].append(target)
+                payload = json.dumps({
+                    "header":"JOINED",
+                    "target":target,
+                    "source":nick
+                })
+                await send_to_room(target,payload)
+            elif header == "PART":
+                target = message_obj["target"]
+                connections[websocket]["rooms"].remove(target)
+                payload = json.dumps({
+                    "header":"PARTED",
+                    "source":nick,
+                    "target":target
+                })
+                await send_to_room(target,payload)
+            elif header == "LIST":
+                payload = json.dumps({
+                    "header":"LIST",
+                    "rooms":rooms
+                })
+                await websocket.send(payload)
+            elif header == "LISTNICKS":
+               target = message_obj["target"]
+               users = list_users(target)
+               payload = json.dumps({
+                   "header":"LISTNICKS",
+                   "users":users
+               })
+               await websocket.send(payload)
     except websockets.exceptions.ConnectionClosed:
         c = connections.pop(websocket)
         nick = c["nick"]
-        for conn in connections.keys():
-            conn.send("{} has disconnected.".format(nick))
+        for room in c["rooms"]:
+            payload = json.dumps({
+                "header":"PARTED",
+                "source":nick,
+                "target":room})
+            await send_to_room(room,payload)
 
 start_server = websockets.serve(connection_handler, 'localhost', 8080)
 
